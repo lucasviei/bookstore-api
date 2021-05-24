a0917c881b3cbbf328c539c05f7526e08a731100
lucasviei <lucassantos110298@gmail.com>
2021-05-24 09:23:11 -0300

    Update para Categoria

--- src/main/java/com/lucas/bookstore/service/CategoriaService.java
+++ src/main/java/com/lucas/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucas.bookstore.domain.Categoria;
+import com.lucas.bookstore.dtos.CategoriaDTO;
 import com.lucas.bookstore.repositories.CategoriaRepository;
 import com.lucas.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -27,6 +28,13 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
 }
