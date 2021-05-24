2d7b9b9f6d67bbd40168512a2c1f2995d1eab7fd
lucasviei <lucassantos110298@gmail.com>
2021-05-24 16:20:56 -0300

    Get para Livro por Id

---
+++ src/main/java/com/lucas/bookstore/service/LivroService.java
@@ -0,0 +1,22 @@
+package com.lucas.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucas.bookstore.domain.Livro;
+import com.lucas.bookstore.repositories.LivroRepository;
+import com.lucas.bookstore.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class LivroService {
+	
+	@Autowired
+	private LivroRepository repository;
+	
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
+	}
+}
